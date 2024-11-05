1ff11501c8be06ecc9bdb65fdff01052a5272310
John Doe <johndoe@exemple.com>
2024-11-05 19:44:18 +0100

    Create a table to enter new tasks

--- src/App.vue
+++ src/App.vue
@@ -5,14 +5,19 @@
 
 <script>
 import Form from "@/components/Form";
+import { ref } from "vue";
 export default {
   name: "App",
   components: {
     Form,
   },
   setup() {
+    let tasks = ref([]);
+
     const saveTask = function (data) {
       console.log("App | saveTask() | data", data);
+      tasks.value = [...tasks.value, { task: data, id: Date.now() }];
+      console.log("App | saveTask() | tasks.value", tasks.value);
     };
     return {
       saveTask,
