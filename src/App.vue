edd8278ec4cb75881d40627f8b15867ad2f3a490
John Doe <johndoe@exemple.com>
2024-11-05 20:54:50 +0100

    Create component TasksList

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,18 @@
 <template>
   <h1>My todo list</h1>
   <Form @add="saveTask" />
-  <ul>
-    <li v-for="taskName in tasks" :key="taskName.id">{{ taskName.task }}</li>
-  </ul>
-  {{ tasks.length }} task{{ tasks.length > 1 ? "s" : "" }}
+  <TasksList :tasks="tasks" />
 </template>
 
 <script>
 import Form from "@/components/Form";
+import TasksList from "@/components/TasksList";
 import { ref } from "vue";
 export default {
   name: "App",
   components: {
     Form,
+    TasksList,
   },
   setup() {
     let tasks = ref([]);
@@ -40,37 +39,10 @@ export default {
   margin-top: 60px;
 }
 
-ul {
-  list-style: none;
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: center;
-  align-items: center;
-  padding: 0;
-  margin: 0;
-  width: 100%;
-  max-width: 1200px;
-  margin: 0 auto;
-  margin-top: 20px;
-}
-
 h1 {
   font-size: 40px;
   margin-bottom: 20px;
 }
 
-li {
-  width: 20%;
-  margin: 10px;
-  padding: 30px;
-  border: 1px solid #2c3e50;
-  border-radius: 5px;
-  background-color: #2c3e50;
-  color: #fff;
-  font-size: 20px;
-  text-align: center;
-  cursor: pointer;
-  word-wrap: break-word;
-  overflow-wrap: break-word;
-}
+
 </style>
---
+++ src/components/TasksList.vue
@@ -0,0 +1,57 @@
+<template>
+  <h3>All my tasks to do</h3>
+  {{ tasks.length }} task{{ tasks.length > 1 ? "s" : "" }}
+  <ul>
+    <li v-for="taskName in tasks" :key="taskName.id">{{ taskName.task }}</li>
+  </ul>
+</template>
+
+<script>
+export default {
+  props: {
+    tasks: {
+      type: Array,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style>
+h3 {
+  font-size: 25px;
+  margin-top: 65px;
+  margin-bottom: 20px;
+}
+
+ul {
+  list-style: none;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
+  padding: 0;
+  margin: 0;
+  width: 100%;
+  max-width: 1200px;
+  margin: 0 auto;
+  margin-top: 20px;
+}
+
+li {
+  width: 20%;
+  margin: 10px;
+  padding: 30px;
+  border: 1px solid #2c3e50;
+  border-radius: 5px;
+  background-color: #2c3e50;
+  color: #fff;
+  font-size: 20px;
+  text-align: center;
+  cursor: pointer;
+  word-wrap: break-word;
+  overflow-wrap: break-word;
+}
+
+
+</style>
